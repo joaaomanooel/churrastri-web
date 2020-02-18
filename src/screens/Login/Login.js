a5f7d65c2fa87e38d15a7122cf402de8c42e75be
João Manoel Neto <joaaomanooel@gmail.com>
2020-02-18 13:08:00 -0300

    update login screen

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { t } from '../../i18n';
-// import { Input, Header, Button, TrincaLogo } from '../../components';
-import { Container, Title, TitleContainer, Input, FormContainer, Button } from './StyledComponent';
+import { Button, Link, Header, Input } from '../../components';
+import { Container, FormContainer, SignUpText } from './StyledComponent';
 
 export default ({ history, login, loading, user }) => {
   const [email, setEmail] = useState();
@@ -10,32 +10,28 @@ export default ({ history, login, loading, user }) => {
 
   const handleLogin = () => login({ email, password });
 
-  useEffect(() => {
-    user._id && history.push('/');
-  }, [user]);
+  useEffect(() => { user._id && history.push('/'); }, [user]);
 
   return (
     <Container>
-      <TitleContainer>
-        <Title>{t('barbecueCalendar')}</Title>
-      </TitleContainer>
+      <Header />
       <FormContainer>
-        {t('email')}
         <Input
-          onChange={({ target }) => setEmail(target.value)}
-          placeholder={t('email')}
           type="email"
+          value={email}
+          label={t('email')}
+          onChange={setEmail}
+          placeholder={t('email')}
         />
-        {t('password')}
         <Input
-          onChange={({ target }) => setPassword(target.value)}
           placeholder={t('password')}
+          onChange={setPassword}
+          label={t('password')}
+          value={password}
           type="password"
         />
-
-        <Button disabled={loading} onClick={handleLogin}>
-          {t('enter')}
-        </Button>
+        <Button disabled={loading} onClick={handleLogin} text={t('enter')} />
+        <SignUpText>{t('isMemberQuestion')} <Link text={t('signUpNow')} /></SignUpText>
       </FormContainer>
     </Container>
   );
--- src/screens/Login/StyledComponent.js
+++ src/screens/Login/StyledComponent.js
@@ -9,48 +9,17 @@ export const Container = styled.div`
   flex: 1;
 `;
 
-export const TitleContainer = styled.div`
-  height: ${layout.screenHeight * 0.4}px;
-  background-color: ${colors.yellow()};
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  padding: 50;
-`;
-
-export const Title = styled.h1`
-  font-size: ${layout.scale() * 36}px;
-  align-self: center;
-`;
-
 export const FormContainer = styled.div`
+  margin-top: ${layout.scale() * -20}px;
   width: ${layout.screenWidth * 0.3}px;
+  justify-content: center;
   flex-direction: column;
+  align-items: center;
   align-self: center;
   display: flex;
 `;
 
-export const Input = styled.input`
-  font-size: ${layout.scale() * 16}px;
-  padding: ${layout.scale() * 10}px;
-  margin: ${layout.scale() * 20}px;
+export const SignUpText = styled.label`
+  font-size: ${layout.scale() * 12}px;
   align-self: center;
-  width: 100%;
-  border: 0;
 `
-
-export const Button = styled.button`
-  border-radius: ${layout.scale() * 15}px;
-  background-color: ${colors.black(0.9)};
-  margin-top: ${layout.scale() * 30}px;
-  font-size: ${layout.scale() * 16}px;
-  padding: ${layout.scale() * 10}px;
-  color: ${colors.white()};
-  align-self: center;
-  cursor: pointer;
-  width: 100%;
-  border: 0;
-  :active {
-    background-color: ${colors.black(0.78)};
-  }
-`;
