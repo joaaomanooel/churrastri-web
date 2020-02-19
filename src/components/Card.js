411b2abf4b571df06eb6f5eccbde1e0acbb9cda4
João Manoel Neto <joaaomanooel@gmail.com>
2020-02-19 00:21:36 -0300

    end card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { format, parseISO } from 'date-fns';
+import { MdSupervisorAccount, MdMonetizationOn } from 'react-icons/md';
 import { colors, layout } from '../constants';
+import { toCurrency } from '../i18n';
 
 const Card = styled.div`
   -webkit-box-shadow: 0px 2px 10px 0px ${colors.black(0.15)};
@@ -13,6 +15,7 @@ const Card = styled.div`
   height: ${layout.scale() * 200}px;
   width: ${layout.scale() * 300}px;
   margin: ${layout.scale() * 15}px;
+  justify-content: space-between;
   flex-direction: column;
   cursor: pointer;
   display: flex;
@@ -21,6 +24,10 @@ const Card = styled.div`
   }
 `;
 
+const TopContainer = styled.div`
+  flex-direction: column;
+  display: flex;
+`;
 const CardDate = styled.h2`
   line-height: ${layout.scale() * 36}px;
   font-size: ${layout.scale() * 28}px;
@@ -37,12 +44,41 @@ const Title = styled.text`
   overflow: hidden;
 `;
 
+const BottomContainer = styled.div`
+  justify-content: space-between;
+  align-content: center;
+  flex-direction: row;
+  align-items: center;
+  display: flex;
+`;
+
+const BottomItemContainer = styled.div`
+  flex-direction: row;
+  display: flex;
+`;
+
+const BottomText = styled.text`
+  padding-left: ${layout.scale() * 5}px;
+`;
+
 export default React.memo(({ data = {} }) => {
   const date = data.date ? parseISO(data.date) : new Date();
   return (
     <Card>
-      <CardDate>{format(date, 'dd/MM')}</CardDate>
-      <Title>{data.title}</Title>
+      <TopContainer>
+        <CardDate>{format(date, 'dd/MM')}</CardDate>
+        <Title>{data.title}</Title>
+      </TopContainer>
+      <BottomContainer>
+        <BottomItemContainer>
+          <MdSupervisorAccount color={colors.yellow()} size={24} />
+          <BottomText>{data.participants && data.participants.length || 0}</BottomText>
+        </BottomItemContainer>
+        <BottomItemContainer>
+          <MdMonetizationOn color={colors.yellow()} size={24} />
+          <BottomText>{toCurrency(data.price || 0)}</BottomText>
+        </BottomItemContainer>
+      </BottomContainer>
     </Card>
   );
 });
