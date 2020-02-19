6121ed184e8fff66143bd22fdc73597efe3d822a
João Manoel Neto <joaaomanooel@gmail.com>
2020-02-18 23:46:30 -0300

    add Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
+import { format, parseISO } from 'date-fns';
 import { colors, layout } from '../constants';
 
-export const Container = styled.div`
-
+const Card = styled.div`
+  -webkit-box-shadow: 0px 2px 10px 0px ${colors.black(0.15)};
+  -moz-box-shadow: 0px 2px 10px 0px ${colors.black(0.15)};
+  box-shadow: 0px 2px 10px 0px ${colors.black(0.15)};
+  border-radius: ${layout.scale() * 2}px;
+  background-color: ${colors.white()};
+  padding: ${layout.scale() * 20}px;
+  height: ${layout.scale() * 200}px;
+  width: ${layout.scale() * 300}px;
+  margin: ${layout.scale() * 15}px;
+  flex-direction: column;
+  cursor: pointer;
+  display: flex;
+  :active {
+    background-color: ${colors.white(0.8)};
+  }
 `;
 
-export default React.memo(() => <Container />);
+const CardDate = styled.h2`
+  line-height: ${layout.scale() * 36}px;
+  font-size: ${layout.scale() * 28}px;
+  justify-content: center;
+  font-weight: 800;
+`;
+
+const Title = styled.text`
+  line-height: ${layout.scale() * 36}px;
+  font-size: ${layout.scale() * 20}px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-weight: 600;
+  overflow: hidden;
+`;
+
+export default React.memo(({ data = {} }) => {
+  const date = data.date ? parseISO(data.date) : new Date();
+  return (
+    <Card>
+      <CardDate>{format(date, 'dd/MM')}</CardDate>
+      <Title>{data.title}</Title>
+    </Card>
+  );
+});
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  Card: require('./Card').default,
   Link: require('./Link').default,
   Input: require('./Input').default,
   Header: require('./Header').default,
