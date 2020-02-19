11a8f063c90fcd0941e966f3908072e3042f9af4
João Manoel Neto <joaaomanooel@gmail.com>
2020-02-18 23:42:35 -0300

    update Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { t } from '../i18n';
 import { colors, layout, images } from '../constants';
 
-export const TitleContainer = styled.div`
+const TitleContainer = styled.div`
   background-image: url(${images.headerImg});
   height: ${layout.scale() * 300}px;
   background-color: ${colors.yellow()};
@@ -13,9 +13,11 @@ export const TitleContainer = styled.div`
   padding: 50;
 `;
 
-export const Title = styled.h1`
+const Title = styled.h1`
+  line-height: ${layout.scale() * 40}px;
   font-size: ${layout.scale() * 36}px;
   align-self: center;
+  font-weight: 800;
 `;
 
 export default React.memo(() => (
